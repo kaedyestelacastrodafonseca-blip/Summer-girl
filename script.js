d33056fa39faa5a360dd2f79c46ad5f58722c279
kaedyestelacastrodafonseca-blip <kaedyestelacastrodafonseca@gmail.com>
2026-09-14 15:32:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert ("JAVASCRIPT FUNCIONANDO");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botaoCarrinho = document.getElementById("carrinho");
+
+    const carrinho = document.createElement("div");
+
+    carrinho.id = "carrinho-menu";
+
+    carrinho.innerHTML = `
+        <h2>Meu carrinho 🛍️</h2>
+        <p>Seu carrinho está vazio.</p>
+        <button id="fechar-carrinho">Fechar</button>
+    `;
+
+    document.body.appendChild(carrinho);
+
+    carrinho.style.display = "none";
+
+    botaoCarrinho.addEventListener("click", function () {
+        carrinho.style.display = "block";
+    });
+
+    document.getElementById("fechar-carrinho").addEventListener("click", function () {
+        carrinho.style.display = "none";
+    });
+
+});
